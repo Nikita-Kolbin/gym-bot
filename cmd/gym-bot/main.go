64818bac3d01fe86b9b3a16dd6d39999706769ba
Nikita-Kolbin <kolbin.niclita2014@yandex.ru>
2024-02-19 22:45:17 +0500

    Add tg client and implement base client methods

--- cmd/gym-bot/main.go
+++ cmd/gym-bot/main.go
@@ -1,11 +1,37 @@
 package main
 
-func main() {
-	// TODO: storage
+import (
+	"flag"
+	"gym-bot/internal/client"
+	"log"
+	"os"
+)
 
-	// TODO: client
+func main() {
+	// В процессе разработки используется переменная
+	// окружения, в релизе будет флаг консоли
+	c := client.New(os.Getenv("TG_BOT_TOKEN"))
+	_ = c
+
+	// TODO: storage
 
 	// TODO: fetcher, processor
 
 	// TODO: consumer
+}
+
+func mustToken() string {
+	token := flag.String(
+		"token",
+		"",
+		"token for access to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("missing token variable")
+	}
+
+	return *token
 }
---
+++ internal/client/client.go
@@ -0,0 +1,88 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
+)
+
+const (
+	tgBotHost         = "api.telegram.org"
+	getUpdatesMethod  = "getUpdates"
+	sendMessageMethod = "sendMessage"
+)
+
+type Client struct {
+	host       string
+	basePath   string
+	httpClient http.Client
+}
+
+func New(token string) *Client {
+	return &Client{
+		host:       tgBotHost,
+		basePath:   newBasePath(token),
+		httpClient: http.Client{},
+	}
+}
+
+func newBasePath(token string) string {
+	return "bot" + token
+}
+
+func (c *Client) Updates(offset, limit int) ([]Update, error) {
+	q := url.Values{}
+	q.Add("offset", strconv.Itoa(offset))
+	q.Add("limit", strconv.Itoa(limit))
+
+	data, err := c.doRequest(getUpdatesMethod, q)
+	if err != nil {
+		return nil, fmt.Errorf("can't get updates: %w", err)
+	}
+
+	var res UpdatesResponse
+
+	if json.Unmarshal(data, &res) != nil {
+		return nil, fmt.Errorf("can't get updates: %w", err)
+	}
+
+	return res.Result, nil
+}
+
+func (c *Client) SendMessage(chatID int, text string) error {
+	q := url.Values{}
+	q.Add("chat_id", strconv.Itoa(chatID))
+	q.Add("text", text)
+
+	if _, err := c.doRequest(sendMessageMethod, q); err != nil {
+		return fmt.Errorf("can't send message: %w", err)
+	}
+
+	return nil
+}
+
+func (c *Client) doRequest(method string, query url.Values) ([]byte, error) {
+	u := url.URL{
+		Scheme:   "https",
+		Host:     c.host,
+		Path:     path.Join(c.basePath, method),
+		RawQuery: query.Encode(),
+	}
+
+	resp, err := c.httpClient.Get(u.String())
+	if err != nil {
+		return nil, fmt.Errorf("can't do request: %w", err)
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("can't do request: %w", err)
+	}
+
+	return body, nil
+}
